71d5bc1664a9f9407c85ee566013bd747472cde7
decimozs <marlonadiguemartin548@gmail.com>
2025-06-17 17:12:52 +0800

    test commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173", "https://binspire-web.onrender.com/"],
+    credentials: true,
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  }),
+);
 
 app.get("/", (c) => {
   return c.text("Hello from Binspire API!");
