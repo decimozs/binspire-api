7c181cfd0a1c6d39e1cc1a72241d6248d573fd90
decimozs <marlonadiguemartin548@gmail.com>
2025-06-17 17:03:43 +0800

    test commit

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,14 @@ app.get("/", (c) => {
   return c.text("Hello from Binspire API!");
 });
 
+app.get("/checkhealth", (c) => {
+  return c.json(
+    {
+      success: true,
+      message: "All systems is normal",
+    },
+    200,
+  );
+});
+
 export default app;
